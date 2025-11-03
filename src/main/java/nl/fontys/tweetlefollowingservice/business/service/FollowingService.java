8cc52b265507d0c3c95efc6f737809aac22df064
erde123 <108157433+erde123@users.noreply.github.com>
2025-11-03 14:13:44 +0100

    add following service

---
+++ src/main/java/nl/fontys/tweetlefollowingservice/business/service/FollowingService.java
@@ -0,0 +1,48 @@
+package nl.fontys.tweetlefollowingservice.business.service;
+
+import lombok.RequiredArgsConstructor;
+import nl.fontys.tweetlefollowingservice.persistence.entity.FollowingEntity;
+import nl.fontys.tweetlefollowingservice.persistence.repository.FollowingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+
+@Service
+public class FollowingService {
+
+    @Autowired
+    private FollowingRepository followingRepository;
+
+    public FollowingEntity followUser(Long followerId, Long followeeId) {
+        if (followerId.equals(followeeId)) {
+            throw new IllegalArgumentException("You cannot follow yourself.");
+        }
+
+        if (followingRepository.existsByFollowingIdAndFolloweeId(followerId, followeeId)) {
+            throw new IllegalStateException("Already following this user.");
+        }
+
+        FollowingEntity entity = FollowingEntity.builder()
+                .followingId(followerId)
+                .followeeId(followeeId)
+                .createdAt(Instant.now().toEpochMilli())
+                .build();
+
+        return followingRepository.save(entity);
+    }
+
+    public void unfollowUser(Long followerId, Long followeeId) {
+        followingRepository.deleteByFollowingIdAndFolloweeId(followerId, followeeId);
+    }
+
+    public List<FollowingEntity> getFollowing(Long followerId) {
+        return followingRepository.findByFollowingId(followerId);
+    }
+
+    public List<FollowingEntity> getFollowers(Long followeeId) {
+        return followingRepository.findByFolloweeId(followeeId);
+    }
+}
+
