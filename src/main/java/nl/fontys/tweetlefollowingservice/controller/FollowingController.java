3d1cafcd45299431320ac10944de79358458360c
erde123 <108157433+erde123@users.noreply.github.com>
2025-11-18 21:04:32 +0100

    update mapping

--- src/main/java/nl/fontys/tweetlefollowingservice/controller/FollowingController.java
+++ src/main/java/nl/fontys/tweetlefollowingservice/controller/FollowingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/follow")
+@RequestMapping("/api/following")
 public class FollowingController {
 
     @Autowired
